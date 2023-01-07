ef1d1b692e8444bf4e4727038c7a8b11e8672e0b
Kamil Marczak <kamil.codemate@gmail.com>
2023-01-07 20:42:41 +0100

    Fixed searching own account

--- php/search.php
+++ php/search.php
@@ -6,7 +6,7 @@ $eq = '';
 
 $searchValue = mysqli_real_escape_string($conn, $_POST['searchValue']);
 
-$qry = mysqli_query($conn, "SELECT * FROM users WHERE name LIKE '%$searchValue%' OR surname LIKE '%$searchValue%' OR CONCAT(name, ' ', surname) LIKE '%$searchValue%'");
+$qry = mysqli_query($conn, "SELECT * FROM users WHERE unique_id != $out_id AND (name LIKE '%$searchValue%' OR surname LIKE '%$searchValue%' OR CONCAT(name, ' ', surname) LIKE '%$searchValue%')");
 
 if(mysqli_num_rows($qry) > 0)
 {
