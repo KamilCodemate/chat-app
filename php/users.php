74b9aa114c226d36cada4c4b2e1fe48871d4f059
Kamil Marczak <kamil.codemate@gmail.com>
2023-01-09 15:18:24 +0100

    Fix users display

--- php/users.php
+++ php/users.php
@@ -7,11 +7,11 @@ $out_id = $_SESSION['unique_id'];
 $qry = mysqli_query($conn, "SELECT * FROM users WHERE unique_id != $out_id");
 $eq = '';
 
-if(mysqli_num_rows($qry) <=  1)
+if(mysqli_num_rows($qry) <= 0)
 {
  $eq = 'Brak użytkowników do wyświetlenia';
 }
-elseif(mysqli_num_rows($qry) > 1)
+elseif(mysqli_num_rows($qry) > 0)
 {
     include "userStructure.php";
     
