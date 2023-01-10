0fcb9a0abd764cb7a456909015e42735e75c4058
Kamil Marczak <kamil.codemate@gmail.com>
2023-01-10 20:55:15 +0100

    Fix last message showing

--- php/userStructure.php
+++ php/userStructure.php
@@ -5,8 +5,7 @@ $eq2 = '';
  while($row = mysqli_fetch_assoc($qry))
  {
     $color = (trim($row['status']) == 'Aktywny Teraz') ? 'rgb(17, 167, 17)' : 'grey';
-    $qry2 = mysqli_query($conn, "SELECT * FROM messages WHERE in_id = {$row['unique_id']} OR out_id = {$row['unique_id']} AND out_id = '$out_id'
-    OR out_id = {$row['unique_id']} ORDER BY id DESC LIMIT 1");
+    $qry2 = mysqli_query($conn, "SELECT * FROM messages WHERE (in_id = {$row['unique_id']} AND out_id = {$_SESSION['unique_id']}) OR (in_id = {$_SESSION['unique_id']} AND out_id = {$row['unique_id']}) ORDER BY id DESC");
 
     $row2 = mysqli_fetch_assoc($qry2);
     if(mysqli_num_rows($qry2) > 0)
